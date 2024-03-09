4cadc1d2bd7fff2dbd7de9b0e3cc08d91a5b5c58
AlloRus <103575145+AlloRus@users.noreply.github.com>
2024-03-09 16:36:58 +0300

    Create users_resource.py

---
+++ users_resource.py
@@ -0,0 +1,58 @@
+from data import db_session
+from data.users import User
+from flask_restful import abort, Resource
+from flask import jsonify
+from .reqparse import parser
+
+def abort_if_users_not_found(users_id):
+    sess = db_session.create_session()
+    users = sess.query(User).get(users_id)
+    if not users:
+        abort(404, message=f"User {users_id} not found")
+
+
+class UsersResource(Resource):
+    def get(self, users_id):
+        abort_if_users_not_found(users_id)
+        sess = db_session.create_session()
+        users = sess.query(User).get(users_id)
+        return jsonify(
+        {
+            'users': users.to_dict(only=(
+                'id', 'name', 'about', 'email'))
+        }
+    )
+
+    def delete(self, users_id):
+        abort_if_users_not_found(users_id)
+        db_sess = db_session.create_session()
+        users = db_sess.query(User).get(users_id)
+        db_sess.delete(users)
+        db_sess.commit()
+        return jsonify({'success': 'OK'})
+
+
+class UsersListResource(Resource):
+    def get(self):
+        db_sess = db_session.create_session()
+        users = db_sess.query(User).all()
+        return jsonify(
+            {
+                'users':
+                    [item.to_dict(only=(''id', 'name', 'about', 'email')) 
+                    for item in users]
+            }
+        )
+
+    def post(self):
+        args = parser.parse_args()
+        db_sess = db_session.create_session()
+        users = User(
+            id=args['id'],
+            name=args['name'],
+            about=args['about'],
+            email=args['email']
+        )
+        db_sess.add(users)
+        db_sess.commit()
+        return jsonify({'success': 'OK'})
