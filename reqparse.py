35f31f3dfe07829691acc3793f5ffec311d0c915
AlloRus <103575145+AlloRus@users.noreply.github.com>
2024-03-09 16:38:21 +0300

    Create reqparse.py

---
+++ reqparse.py
@@ -0,0 +1,7 @@
+from flask_restful import reqparse
+
+parser = reqparse.RequestParser()
+parser.add_argument("id", required=True, type=int)
+parser.add_argument("name", required=True)
+parser.add_argument("about")
+parser.add_argument("email", required=True)
