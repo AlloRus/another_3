ed7dc45d4eba27015504f3768192b70309bb3024
AlloRus <103575145+AlloRus@users.noreply.github.com>
2024-03-09 16:52:10 +0300

    Update reqparse.py

--- reqparse.py
+++ reqparse.py
@@ -3,5 +3,6 @@ from flask_restful import reqparse
 parser = reqparse.RequestParser()
 parser.add_argument("id", required=True, type=int)
 parser.add_argument("name", required=True)
-parser.add_argument("about")
+parser.add_argument("hashed_password", required=True)
 parser.add_argument("email", required=True)
+parser.add_argument("created_date", required=True)
